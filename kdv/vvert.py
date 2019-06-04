c246317842aea0f007e3cb5f993f0a4edb47f4dd
Connor Duffin <connor.p.duffin@gmail.com>
2019-06-04 16:41:25 +0800

    Updated VVertical-mode to include all new parameter computations

--- kdv/vvert.py
+++ kdv/vvert.py
@@ -3,31 +3,39 @@ import logging
 import numpy as np
 import scipy.sparse as sparse
 
+from scipy.interpolate import interp1d
 from scipy.linalg import eigh
 from scipy.sparse.linalg import eigs
 
 
-class VerticalMode(object):
-
-    def __init__(self, dx, start_x, end_x, dz, start_z, end_z, rho_0):
-        self.dz = dz
-        self.z_grid = np.arange(start_z, end_z, dz)
-        self.n_z = len(self.z_grid)
-        self.dx = dz
-        self.x_grid = np.arange(start_z, end_z, dz)
-        self.n_x = len(self.z_grid)
+class VVerticalMode(object):
+    def __init__(self, dx, start_x, end_x, dz0, start_z0, end_z0, n_interp,
+                 rho_0):
+        self.dz0 = dz0
+        self.z_grid = np.arange(start_z0, end_z0, dz0)
+        self.n_z0 = len(self.z_grid)
+        self.dx = dx
+        self.x_grid = np.arange(start_x, end_x, dx)
+        self.n_x = len(self.x_grid)
+        self.x_subset = list(
+            range(0, len(self.x_grid), int(len(self.x_grid) / n_interp))
+        ) + [len(self.x_grid) - 1]
 
         self.rho_0 = rho_0
-
+        self.bathymetry = None
         self.density = None
-        self.grad_density = None
+        self.density_grad = None
 
-        self.phi = np.zeros((self.n_x, self.n_z))
-        self.phi_grad = np.zeros((self.n_x, self.n_z))
+        self.phi0 = np.zeros((self.n_x, self.n_z0))
+        self.phi0_grad = np.zeros((self.n_x, self.n_z0))
         self.c = np.zeros((self.n_x, 1))
 
         self.r10 = np.zeros((self.n_x, 1))
         self.r01 = np.zeros((self.n_x, 1))
+        self.q = np.zeros((self.n_x, 1))
+
+    def compute_bathymetry(self, ocean_floor):
+        self.bathymetry = self.z_grid[-1] - ocean_floor
 
     def compute_density(self, density="sech"):
         z_grid = self.z_grid
@@ -56,36 +64,86 @@ class VerticalMode(object):
         else:
             logging.INFO("Please try another density (e.g. 'lamb-yan-1')")
             logging.ERROR("Density not initialized")
-        self.grad_density = np.gradient(self.density, self.dz)
+        self.density_grad = np.gradient(self.density, self.dz0)
 
-    def find_vertical_modes(self):
-        dz = self.dz
-        n_z = self.n_z
-        dx = self.dx
-        n_x = self.n_x
+    ########################################
+    def compute_parameters(self):
+        dz = self.dz0
         rho_0 = self.rho_0
-        grad_density = self.grad_density
 
-        second_diff = - 1 / (dz**2) * (
-            np.diag(np.full(n_z - 1, 1), k=-1)
-            + np.diag(np.full(n_z, -2), k=0)
-            + np.diag(np.full(n_z - 1, 1), k=1)
-        )
-        scale = np.diag(np.full(n_z, (- 9.81 / rho_0) * grad_density))
+        x_subset = self.x_subset
+        x_grid = self.x_grid
+        z_grid = self.z_grid
+        bathymetry = self.bathymetry
+        density = self.density
 
-        eigenvalue, phi = eigh(
-            second_diff, b=scale, eigvals=(0, 0)
-        )
-        self.phi = np.ndarray.flatten(phi)
-        self.phi /= np.max(self.phi)
-        self.phi_grad = np.gradient(self.phi, self.dz)
-        self.c = np.sqrt(1 / eigenvalue[0])
+        c_temp = np.zeros(len(x_subset))
+        q_temp = np.zeros(len(x_subset))
+        r01_temp = np.zeros(len(x_subset))
+        r10_temp = np.zeros(len(x_subset))
+
+        for i in range(len(x_subset)):
+            # select the z-grid of interest
+            z_grid_temp = z_grid[z_grid <= bathymetry[x_subset[i]]]
+            n_z = len(z_grid_temp)
+            # make sure we are getting enough points in our domain
+            print(n_z)
+
+            # compute the density on the z-grid
+            density_temp = density[range(0, len(z_grid_temp))]
+            density_grad_temp = np.gradient(density_temp, dz)
+
+            # finite diff matrix on the z-grid
+            second_diff_temp = - 1 / (dz**2) * (
+                np.diag(np.full(n_z - 1, 1), k=-1)
+                + np.diag(np.full(n_z, -2), k=0)
+                + np.diag(np.full(n_z - 1, 1), k=1)
+            )
+            scale = np.diag(np.full(n_z, (- 9.81 / rho_0) * density_grad_temp))
+
+            # eigh faster than any sparse stuff
+            eigenvalue, phi = eigh(
+                second_diff_temp, b=scale, eigvals=(0, 0)
+            )
+            phi = np.ndarray.flatten(phi)
+            phi /= np.max(phi)
+            phi_grad = np.gradient(phi, dz)
+
+            # set the initial element (to be used in computing Q)
+            if (i == 0):
+                phi0_grad = phi_grad
+
+            # compute c, r10, r01, Q
+            c_temp[i] = np.sqrt(1 / eigenvalue[0])
+            r10_temp[i] = (
+                (3 * c_temp[i] / 2) * (np.trapz(np.power(phi_grad, 3), dx=dz)
+                / np.trapz(np.power(phi_grad, 2), dx=dz))
+            )
+            r01_temp[i] = (
+                (c_temp[i] / 2) * (np.trapz(np.power(phi, 2), dx=dz)
+                / np.trapz(np.power(phi_grad, 2), dx=dz))
+            )
+            q_temp[i] = np.sqrt(
+                c_temp[0]**3 * np.trapz(np.power(phi0_grad, 2), dx=dz)
+                / (c_temp[i]**3 * np.trapz(np.power(phi_grad, 2), dx=dz))
+            )
+
+        # interpolate out the other parameters, from the grid
+        f_c = interp1d(x_grid[x_subset], c_temp)
+        f_r01 = interp1d(x_grid[x_subset], r01_temp)
+        f_r10 = interp1d(x_grid[x_subset], r10_temp)
+        f_q = interp1d(x_grid[x_subset], q_temp)
+
+        self.c = f_c(x_grid)
+        self.r01 = f_r01(x_grid)
+        self.r10 = f_r10(x_grid)
+        self.q = f_q(x_grid)
 
     def find_vertical_mode_sparse(self):
         dz = self.dz
         n_z = self.n_z
         rho_0 = self.rho_0
-        grad_density = self.grad_density
+        density_grad = self.density_grad
 
         second_diff = - 1 / (dz**2) * sparse.diags(
             diagonals=[
@@ -97,7 +155,7 @@ class VerticalMode(object):
             format="csc"
         )
         scale = sparse.diags(
-            np.full(n_z, (9.81 / rho_0) * grad_density), 0, format="csc"
+            np.full(n_z, (9.81 / rho_0) * density_grad), 0, format="csc"
         )
 
         eigenvalue, phi = eigs(second_diff, b=scale, k=1, which="LM")
