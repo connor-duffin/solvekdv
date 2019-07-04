f867dd5b9db2c13a2211624b1ea17a53f962310e
Connor Duffin <connor.p.duffin@gmail.com>
2019-07-04 20:10:06 +0800

    Removed interpolation from vvert module

--- solvekdv/vvert.py
+++ solvekdv/vvert.py
@@ -1,12 +1,11 @@
 import logging
 
 import numpy as np
-# import scipy.interpolate as interpolate
 import scipy.linalg as la
 
 
 class VVerticalMode(object):
-    def __init__(self, dx, start_x, end_x, dz0, start_z0, end_z0, n_interp,
+    def __init__(self, dx, start_x, end_x, dz0, start_z0, end_z0, n_eigen,
                  rho_0):
         self.dz0 = dz0
         self.z0_grid = np.arange(start_z0, end_z0, dz0)
@@ -14,13 +13,13 @@ class VVerticalMode(object):
         self.dx = dx
         self.x_grid = np.arange(start_x, end_x, dx)
         self.n_x = len(self.x_grid)
-        self.x_subs_index = np.round(
-            np.linspace(0, self.n_x - 1, n_interp)
-        ).astype(int)
+        self.n_eigen = n_eigen
+
         self.rho_0 = rho_0
         self.bathymetry = None
         self.density = None
         self.density_grad = None
+        self.density_func = None
 
         self.phi0 = np.zeros((self.n_x, self.n_z0))
         self.phi0_grad = np.zeros((self.n_x, self.n_z0))
@@ -34,29 +33,34 @@ class VVerticalMode(object):
     def compute_bathymetry(self, ocean_floor):
         self.bathymetry = self.z0_grid[-1] - ocean_floor
 
-    def compute_density(self, density="sech"):
+    def compute_density(self, density="tanh"):
+        """ Compute densities using three predefined functions.
+        The function that you use is stored for compute_parameters()
+        """
         z0_grid = self.z0_grid
-        if (
-            isinstance(density, np.ndarray)
-            and (
-                density.shape == (self.n_z0, )
-                or density.shape == (self.n_z0, 1)
-            )
-        ):
-            self.density = density
-        elif density == "lamb-yan-1":
+        if density == "lamb-yan-1":
             self.density = (
                 1027.31 - 3.3955 * np.exp((z0_grid - 300) / 50)
+            )
+            self.density_func = lambda z: (
+                1027.31 - 3.3955 * np.exp((z - 300) / 50)
             )
         elif density == "lamb-yan-2":
             self.density = (
                 1027.31 - 10**(-4) * z0_grid / 9.81
                 - 1.696 * (1 + np.tanh((z0_grid - 200)/40))
             )
+            self.density_func = lambda z: (
+                1027.31 - 10**(-4) * z / 9.81
+                - 1.696 * (1 + np.tanh((z - 200)/40))
+            )
         elif density == "tanh":
             self.density = (
                 (np.exp(z0_grid) - np.exp(-z0_grid))
                 / (np.exp(z0_grid) + np.exp(-z0_grid))
+            )
+            self.density_func = lambda z: (
+                (np.exp(z) - np.exp(-z)) / (np.exp(z) + np.exp(-z))
             )
         else:
             logging.INFO("Please try another density (e.g. 'lamb-yan-1')")
@@ -64,48 +68,49 @@ class VVerticalMode(object):
         self.density_grad = np.gradient(self.density, self.dz0)
 
     def compute_parameters(self):
+        """ Compute parameters using the eigenvalue problem.
+        All parameters are saved as column vectors for appropriate matrix
+        multiplications.
+        """
         dz = self.dz0
         rho_0 = self.rho_0
+        n_eigen = self.n_eigen
 
-        x_subs_index = self.x_subs_index
-        x_grid = self.x_grid
-        z0_grid = self.z0_grid
         bathymetry = self.bathymetry
-        density_grad = self.density_grad
 
-        c_temp = np.zeros(len(x_subs_index))
-        q_temp = np.zeros(len(x_subs_index))
-        beta_temp = np.zeros(len(x_subs_index))
-        alpha_temp = np.zeros(len(x_subs_index))
+        c_temp = np.zeros(self.n_x)
+        q_temp = np.zeros(self.n_x)
+        beta_temp = np.zeros(self.n_x)
+        alpha_temp = np.zeros(self.n_x)
 
-        for i in range(len(x_subs_index)):
-            # print(f"\rIteration {i + 1:5}", end="")
-            # select the z-grid of interest
-            z_grid_temp = z0_grid[
-                z0_grid < bathymetry[x_subs_index[i]]
-            ]
-            print(z_grid_temp[-1])
-            n_z = len(z_grid_temp)
+        for i in range(self.n_x):
+            # set the z-grid of interest
+            z_grid_temp = np.linspace(0, bathymetry[i], n_eigen)
 
-            # extract the density gradient on the z-grid
-            density_grad_temp = density_grad[range(0, len(z_grid_temp))]
+            # compute the density on the z-grid
+            density_temp = self.density_func(z_grid_temp)
+            density_grad_temp = np.gradient(density_temp)
 
-            # finite diff matrix on the z-grid
+            # lhs of eigenvalue problem
             second_diff_temp = - 1 / (dz**2) * (
-                np.diag(np.full(n_z - 1, 1), k=-1)
-                + np.diag(np.full(n_z, -2), k=0)
-                + np.diag(np.full(n_z - 1, 1), k=1)
+                np.diag(np.full(n_eigen - 1, 1), k=-1)
+                + np.diag(np.full(n_eigen, -2), k=0)
+                + np.diag(np.full(n_eigen - 1, 1), k=1)
             )
+            # rhs of eigenvalue problem
             scale = np.diag(
                 (- 9.81 / rho_0) * density_grad_temp
             )
 
+            # solve eigenvalue problem using dense matrices, get first
             eigenvalue, phi = la.eigh(
                 second_diff_temp, b=scale, eigvals=(0, 0)
             )
             phi = np.ndarray.flatten(phi)
             phi /= np.max(phi)
-            phi_grad = np.gradient(phi, dz)
+
+            # change this part
+            phi_grad = np.gradient(phi, z_grid_temp[1] - z_grid_temp[0])
 
             # set the initial element (to be used in computing q)
             if (i == 0):
@@ -123,16 +128,10 @@ class VVerticalMode(object):
                 (c_temp[i] / 2) * np.trapz(np.power(phi, 2), dx=dz)
                 / np.trapz(np.power(phi_grad, 2), dx=dz)
             )
-            q_temp[i] = np.sqrt(
+            q_temp[i] = (
                 c_temp[0]**3 * np.trapz(np.power(phi0_grad, 2), dx=dz)
                 / (c_temp[i]**3 * np.trapz(np.power(phi_grad, 2), dx=dz))
             )
-
-#         # interpolate the parameters from the grid of values
-#         f_c = interpolate.interp1d(x_grid[x_subs_index], c_temp)
-#         f_beta = interpolate.interp1d(x_grid[x_subs_index], beta_temp)
-#         f_alpha = interpolate.interp1d(x_grid[x_subs_index], alpha_temp)
-#         f_q = interpolate.interp1d(x_grid[x_subs_index], q_temp)
 
         self.c = c_temp
         self.beta = beta_temp
