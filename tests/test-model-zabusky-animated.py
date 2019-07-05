2c000a8a7cdb87bb663f92ebd7b0352bf9e507af
Connor Duffin <connor.p.duffin@gmail.com>
2019-07-05 08:35:43 +0800

    Cleaned up all zabusky test files

--- tests/test-model-zabusky-animated.py
+++ tests/test-model-zabusky-animated.py
@@ -6,24 +6,36 @@ from context import kdv
 
 
 test = kdv.Kdv(
-    dt=0.001, dx=0.001, start_x=0, end_x=2, start_t=0, end_t=20
+    dt=0.001, dx=0.001, start_x=0, end_x=2, start_t=0, end_t=10
+)
+test.set_initial_condition(
+    np.array(np.cos(2 * np.pi * test.x_grid / 2), ndmin=2).T
 )
 
-test.set_initial_condition(np.cos(2 * np.pi * test.x_grid / 2))
-test.set_kdv_parameters(a=1, b=0.022**2, c=0)
+test.a = 1
+test.b = 0.022**2
+test.c = 0
 test.set_first_order_matrix()
 test.set_third_order_matrix()
 test.set_lhs_matrix()
 
 u = np.zeros([test.n_x, test.n_t])
 for i in range(test.n_t):
-    if (i % int(0.1 * test.n_t)) == 0:
-        print(f"Simulation {100 * i / test.n_t:.1f} % complete.")
+    print(f"\rIteration {i + 1:5} / {test.n_t}", end="")
     u[:, i] = test.solve_step()
+
+print()
 
 fig = plt.figure()
 ax = plt.axes(xlim=(0, 2), ylim=(-2, 4))
 line, = plt.plot([], [])
+plt.xlabel("$x$ (distance)")
+plt.ylabel("$A$ (amplitude)")
+plt.title(
+    "KdV Zabusky-Kruskal solution\n" +
+    f"(parameters: a = {test.a:.5f}, b = {test.b:.5f}, c = {test.c:.5f})\n"
+    f"(grid: {test.n_x} $\\times$ {test.n_t} ($x \\times t$))"
+)
 
 
 def init():
--- tests/test-model-zabusky.py
+++ tests/test-model-zabusky.py
@@ -5,31 +5,33 @@ from context import kdv
 
 
 test = kdv.Kdv(
-    dt=0.001, dx=0.005, start_x=0, end_x=2, start_t=0, end_t=10
+    dt=0.01, dx=0.01, start_x=0, end_x=1, start_t=0, end_t=10
 )
-
-test.set_initial_condition(np.cos(np.pi * test.x_grid))
-test.set_kdv_parameters(a=1, b=0.022**2, c=0)
+test.set_initial_condition(
+    np.array(np.cos(2 * np.pi * test.x_grid), ndmin=2).T
+)
+test.a = 1
+test.b = 0.022**2
+test.c = 0
 test.set_first_order_matrix()
 test.set_third_order_matrix()
 test.set_lhs_matrix()
 
-u = np.zeros([test.n_x, test.n_t])
+u = np.zeros((test.n_x, test.n_t))
 for i in range(test.n_t):
-    if (i % int(0.1 * test.n_t)) == 0:
-        print(f"Simulation {100 * i / test.n_t:.1f} % complete.")
+    print(f"\rIteration {i + 1:5} / {test.n_t}", end="")
     u[:, i] = test.solve_step()
+
+print()
 
 xmesh, ymesh = np.meshgrid(test.x_grid, test.t_grid)
 
 plt.figure()
-plt.pcolormesh(xmesh, ymesh, u.transpose(), cmap='viridis')
+plt.pcolormesh(xmesh, ymesh, u.transpose(), cmap="RdBu_r")
 plt.colorbar()
-plt.xlabel('x')
-plt.ylabel('t')
+plt.xlabel("x")
+plt.ylabel("t")
 plt.title(
-    'KdV, ($a$, $b$, $c$)=(%g, %g, %g)' % (
-        test.a, test.b, test.c
-    )
+    f"KdV, ($a$, $b$, $c$)=({test.a:.5f}, {test.b:.5f}, {test.c:.5f})"
 )
 plt.show()
