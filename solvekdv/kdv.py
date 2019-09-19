7ef68785975bf121bfb6cf11acb54867c3ed4080
Connor Duffin <connor.p.duffin@gmail.com>
2019-09-19 10:11:43 +0800

    Added implicit euler step to the solver

--- solvekdv/kdv.py
+++ solvekdv/kdv.py
@@ -61,7 +61,7 @@ class Kdv(object):
         )
         self.third_order_matrix = output
 
-    def set_lhs_matrix(self):
+    def set_imex_lhs_matrix(self):
         output = (
             sparse.identity(self.n_x, format="csr")
             + self.b * (3 * self.dt / 4) * self.third_order_matrix
@@ -69,7 +69,7 @@ class Kdv(object):
         )
         self.lhs_matrix = output
 
-    def solve_step(self):
+    def solve_step_imex(self):
         a, b, c, dt = self.a, self.b, self.c, self.dt
         rhs_vector = (
             self.u0
@@ -84,3 +84,63 @@ class Kdv(object):
         self.u1 = self.u0.copy()
         self.u0 = output.reshape(self.n_x, 1).copy()
         return(output)
+
+    def _im_euler_lhs(self, u, u_prev):
+        dt = self.dt
+        a = self.a
+        b = self.b
+        c = self.c
+        D_first = self.first_order_matrix
+        D_third = self.third_order_matrix
+        return(
+            u - u_prev
+            + dt * a * np.multiply(u, D_first @ u)
+            + dt * b * D_third @ u
+            + dt * c * D_first @ u
+        )
+
+    def _im_euler_jacobian_lhs(self, u):
+        D_first = self.first_order_matrix
+        D_third = self.third_order_matrix
+        dt = self.dt
+        dx = self.dx
+        n_x = self.n_x
+        a = self.a
+        b = self.b
+        c = self.c
+        return(
+
+            sparse.eye(n_x)
+            + a * (dt / (2 * dx)) * sparse.diags(
+                diagonals=[
+                    u[-1],
+                    -u[1:],
+                    np.roll(u, -1) - np.roll(u, 1),
+                    u[:-1],
+                    -u[0]
+                ],
+                offsets=[-(n_x - 1), -1, 0, 1, n_x - 1],
+                format="csc"
+            )
+            + b * dt * D_third
+            + c * dt * D_first
+        )
+
+    def _newton_im_euler(self, tol=1e-8, max_iter=10_000):
+        u_prev = self.u0.copy()
+        u = self.u0.copy()
+        epsilon = 1e6
+        i = 0
+        while (epsilon >= tol and i < max_iter):
+            jacobian = self._im_euler_jacobian_lhs(u)
+            f = self._im_euler_lhs(u, u_prev)
+            s = spla.spsolve(jacobian, -f)
+            u += s
+            i += 1
+            epsilon = np.sqrt(np.sum(s ** 2))
+
+    def _solve_step_im_euler(self):
+        u = self._newton_im_euler()
+        self.u2 = self.u1.copy()
+        self.u1 = self.u0.copy()
+        self.u0 = u.copy()
